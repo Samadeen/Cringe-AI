83f9d4d1041425fb32f50af20d04d9871eab1a87
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-02-09 13:31:05 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,79 @@
+'use client';
+
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from './page.module.css';
+import cringeImg from '../assets/images/cringe.avif';
+import { Form } from 'react-bootstrap';
+import { FormEvent, useState } from 'react';
+import Button from 'react-bootstrap/esm/Button';
+import Spinner from 'react-bootstrap/esm/Spinner';
+import { encode } from 'punycode';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  const [quote, setQuote] = useState('');
+  const [quoteLoading, setQuoteLoading] = useState(false);
+  const [quoteLoadingError, setQuoteLoadingError] = useState(false);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const prompt = formData.get('prompt')?.toString().trim();
+
+    if (prompt) {
+      try {
+        setQuote('');
+        setQuoteLoadingError(false);
+        setQuoteLoading(true);
+
+        const response = await fetch(
+          `/api/cringe?prompt=` + encodeURIComponent(prompt)
+        );
+        const body = await response.json();
+        setQuote(body.quote);
+      } catch (error) {
+        console.log(error);
+        setQuoteLoadingError(true);
+      } finally {
+        setQuoteLoading(false);
+      }
+    }
+  };
+
+  return (
+    <main className={styles.main}>
+      <h1>Cringe AI</h1>
+      <h2>Powered by GPT-3</h2>
+      <p>
+        Enter a topic and the AI will generate a super cringy motivational quote
+      </p>
+      <div className={styles.container}>
+        <Image
+          src={cringeImg}
+          alt='cringe image'
+          fill
+          priority
+          className={styles.mainImage}
+        />
+      </div>
+      <Form onSubmit={handleSubmit} className={styles.inputForm}>
+        <Form.Group className='mb-3' controlId='prompt-input'>
+          <Form.Label>Create a cringy quote about...</Form.Label>
+          <Form.Control
+            name='prompt'
+            placeholder='e.g success, fear, potatoes'
+            maxLength={100}
+          />
+        </Form.Group>
+        <Button type='submit' className='mb-3' disabled={quoteLoading}>
+          Make me cringe
+        </Button>
+      </Form>
+      {quoteLoading && <Spinner animation='border' />}
+      {quoteLoadingError && 'Something went wrong, Please try again'}
+      {quote && <h5>{quote}</h5>}
+    </main>
+  );
 }
